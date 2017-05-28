e41a962c8c37241a6007721d8e61f5310cf8900f
gsmith98 <smitgw11@gmail.com>
2017-05-27 18:15:53 -0700

    webpack setup working. npm run webpack to build, open index.html in browser

---
+++ reactApp/app.js
@@ -0,0 +1,7 @@
+// npm install --save react react-dom babel-core webpack babel-loader babel-preset-es2015 babel-preset-react
+
+import React, { Component } from 'react';
+import ReactDom from 'react-dom';
+
+
+ReactDom.render(<p>Can you see me?</p>, document.getElementById('root'));
