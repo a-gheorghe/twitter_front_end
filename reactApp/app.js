5fb8a1ec3ab1001b3a34eb18861c7b167ad71c33
Graham Smith <smitgw11@gmail.com>
2017-06-09 22:14:12 -0500

    Restored to a working TODOAPP

--- reactApp/app.js
+++ reactApp/app.js
@@ -53,6 +53,37 @@ class TodoApp extends Component {
   }
 }
 
+class InputLine extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { task: '' };
+  }
+
+  handleChange(event) {
+    this.setState({ task: event.target.value });
+  }
+
+  handleSubmit() {
+    this.props.addTodo(this.state.task);
+    this.setState({ task: '' });
+  }
+
+  render() {
+    return (
+      <div>
+        <input
+          type="text"
+          placeholder="task"
+          onChange={(event) => this.handleChange(event)}
+          value={this.state.task}
+        />
+        <button onClick={() => this.handleSubmit()}>Add Todo</button>
+      </div>
+    )
+  }
+}
+
 
 class TodoList extends Component {
   render() {
